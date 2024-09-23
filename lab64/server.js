9bc4f69e909937c2148e90f9fc1b680acf633dca
HelenaLaeth <sophiahelena.leite@outlook.com>
2024-09-23 20:00:13 -0300

    Adicionando o Server.JS

---
+++ lab64/server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/sobre', (req,res) =>{
+  res.sendFile(path.join(__dirname, 'public', 'sobre.html'));
+});
+
+app.use((req, res) => {
+  res.end("404");
+});
+
+// Inicia o servidor
+app.listen(3000, function(){
+  console.log(`Servidor rodando na porta 3000`);
+});
+
